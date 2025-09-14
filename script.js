3e7c919c534c0848dd5a292031feef7dce5f5802
4pd4stantone <victor.stanton@gmail.com>
2025-09-13 20:31:57 -0400

    finished Part 2 Prime Time

--- script.js
+++ script.js
@@ -8,24 +8,47 @@
 
 // Part 1. 
 
-let x = 1;
+// let x = 1;
 
-while (x <= 100) {
-    // console.log(x)
+// while (x <= 100) {
+//     // console.log(x)
     
-    if (x % 3 === 0 && x % 5 === 0){
-        console.log(`Fizz Buzz`)
-    } else if (x % 3 === 0) {
-        console.log(`Fizz`)
-    } else if (x % 5 === 0) {
-        console.log(`Buzz`)
-    } else {
-        console.log(x)
-    }
-    
+//     if (x % 3 === 0 && x % 5 === 0){
+//         console.log(`Fizz Buzz`)
+//     } else if (x % 3 === 0) {
+//         console.log(`Fizz`)
+//     } else if (x % 5 === 0) {
+//         console.log(`Buzz`)
+//     } else {
+//         console.log(x)
+//     }
 
-    x++;
+//     x++;
+// }
+
+
+// Part 2 Prime Time
+
+let n = 17;
+
+for (let i = n+1; i <= 100; i++) {
+  let isPrime = true;
+  if (i >=2 ) {
+    for (let j = i - 1; j >= 2; j--) {
+    if (i % j === 0) { 
+        isPrime = false; 
+        break; }
+  }
+  if (isPrime) {
+    console.log(i);
+    break;
+  }
+  }
+  
 }
+
+    
+
 
 
 
